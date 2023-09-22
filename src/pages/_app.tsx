ce318015593e736b58838437cef9b62315abb484
Kang Minsu <skymin0417@gmail.com>
2023-09-22 20:07:13 +0900

    init: add swr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { SWRConfig } from "swr";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
