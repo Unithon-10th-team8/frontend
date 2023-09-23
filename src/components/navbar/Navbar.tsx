c2fbb1a287602ebc1bacd8be647aae5dce778657
jaychang99 <jaychang99@gmail.com>
2023-09-23 16:46:21 +0900

    feat(Navbar): 내비게이션 바 개발

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import { NAVBAR_MENU_LIST } from "@/components/navbar/constants/navbarMenuList";
+import classNames from "classnames";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="absolute bottom-0 mx-auto my-0 w-full bg-[#232323] px-20 pb-[27px] pt-[10px]">
+      <ul className="flex justify-center gap-16">
+        {NAVBAR_MENU_LIST.map((menu, index) => {
+          const IconComponent = menu.icon;
+          const isActive = router.pathname === menu.path;
+
+          return (
+            <Link
+              href={menu.path}
+              key={index}
+              className="flex min-w-[60px] flex-col items-center"
+            >
+              <li key={index}>
+                <IconComponent isActive={isActive} />
+              </li>
+              <span
+                className={classNames({
+                  "mt-3 text-[11px]": true,
+                  "text-[#565656]": !isActive,
+                  "text-[white]": isActive,
+                })}
+              >
+                {menu.label}
+              </span>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "@/styles/globals.css";
 import { SWRConfig } from "swr";
 import type { AppProps } from "next/app";
-import { AppLayout, ToastProvider } from "@/components";
+import { AppLayout, Navbar, ToastProvider } from "@/components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig>
       <AppLayout>
         <Component {...pageProps} />
+        <Navbar />
       </AppLayout>
       <ToastProvider />
     </SWRConfig>
