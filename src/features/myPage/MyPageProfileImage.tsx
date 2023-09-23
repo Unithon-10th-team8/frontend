1c5b858d2e7568d1a535d74c1ef6bff478b4936d
Kang Minsu <skymin0417@gmail.com>
2023-09-24 03:08:22 +0900

    feat: add mypage api integration & add login middleware

--- src/features/myPage/MyPageProfileImage.tsx
+++ src/features/myPage/MyPageProfileImage.tsx
@@ -1,18 +1,19 @@
 import { CONTACT_DETAIL_IMAGE } from "@/constants";
+import { UserOutput } from "@/fetchers/user";
 import Image from "next/image";
 import { useWatch } from "react-hook-form";
 
 export type MyPageProfileImageProps = {};
 
 export const MyPageProfileImage = ({}: MyPageProfileImageProps) => {
-  const { logo } = useWatch<any>();
+  const { profile_image_url } = useWatch<UserOutput>();
 
   return (
     <div className="flex h-[166px] w-full flex-col items-center justify-center">
       <div className="relative h-[126px] w-[126px] rounded-full">
         <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-full bg-surface">
-          {logo ? (
-            <Image fill src={logo} alt="프로필이미지" />
+          {profile_image_url ? (
+            <Image fill src={profile_image_url} alt="프로필이미지" />
           ) : (
             <Image
               width={88}
