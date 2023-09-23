b9528f578cb1ee4c46bbc6413f5c299b449618d0
jaychang99 <jaychang99@gmail.com>
2023-09-23 19:02:00 +0900

    feat(Calendar): 캘린더 좌우 월 이동 기능 추가

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,17 +1,35 @@
 import { CalendarDay } from "@/components/calendar/items/CalendarDay";
+import { IconCalendarChevronLeft } from "@/components/calendar/items/IconCalendarChevronLeft";
+import { IconCalendarChevronRight } from "@/components/calendar/items/IconCalendarChevronRight";
 import { getCalendarDays } from "@/components/calendar/modules/getCalendarDays";
 import dayjs from "dayjs";
+import { useState } from "react";
 
 export const Calendar = () => {
-  const initialDate = dayjs();
+  const [initialDate, setInitialDate] = useState(dayjs());
 
   const days = getCalendarDays(initialDate);
   const monthText = initialDate.format("MMM");
+
+  const handleClickPreviousMonth = () => {
+    setInitialDate(initialDate.subtract(1, "month"));
+  };
+
+  const handleClickNextMonth = () => {
+    setInitialDate(initialDate.add(1, "month"));
+  };
+
   return (
     <div className="px-20">
-      <p className="size-21 mb-24 mt-[14px] text-center text-[21px] font-medium">
-        {monthText}
-      </p>
+      <div className="mb-24 mt-[14px] flex  items-center justify-center gap-8 ">
+        <button onClick={handleClickPreviousMonth}>
+          <IconCalendarChevronLeft />
+        </button>
+        <p className="size-21 text-[21px] font-medium">{monthText}</p>
+        <button onClick={handleClickNextMonth}>
+          <IconCalendarChevronRight />
+        </button>
+      </div>
       <div className="grid grid-cols-7 gap-1">
         {days.map((date) => {
           const isCurrentMonth = date.isSame(initialDate, "month");
---
+++ src/components/calendar/items/IconCalendarChevronLeft.tsx
@@ -0,0 +1,19 @@
+export const IconCalendarChevronLeft = () => {
+  return (
+    <svg
+      width="17"
+      height="17"
+      viewBox="0 0 17 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.05 2.9751L5.95 8.5001L11.05 14.0251"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
