42125a0502c2a16f376bf686621eb502264c344f
jaychang99 <jaychang99@gmail.com>
2023-09-23 13:25:03 +0900

    feat(Contact): 전체 드롭다운 구현

--- src/features/contacts/components/header/HeaderCategorySelector.tsx
+++ src/features/contacts/components/header/HeaderCategorySelector.tsx
@@ -1,16 +1,44 @@
 import Image from "next/image";
 import { CONTACT_IMAGES } from "@/constants";
+import { Menu } from "@headlessui/react";
+import { HEADER_CATEGORY_LIST } from "@/features/contacts/constants/headerCategoryList";
+import { DividerSelector } from "@/features/contacts/components/header/selector/DividerSelector";
 
 export const HeaderCategorySelector = () => {
   return (
-    <div className="p-x-[4px] p-y-[2px] flex items-center">
-      <span className="mr-[12px] text-[20px] font-bold">전체</span>
-      <Image
-        height={6}
-        width={12}
-        alt="iconChevronDown"
-        src={CONTACT_IMAGES.iconChevronDown}
-      />
+    <div className="p-x-[4px] p-y-[2px] relative flex items-center">
+      <Menu>
+        <Menu.Button>
+          <div className="p-x-[4px] p-y-[2px] flex items-center">
+            <span className="mr-[12px] text-[20px] font-bold">전체</span>
+            <Image
+              height={6}
+              width={12}
+              alt="iconChevronDown"
+              src={CONTACT_IMAGES.iconChevronDown}
+            />
+          </div>
+        </Menu.Button>
+        <Menu.Items className="absolute top-[40px] z-[10] flex flex-col rounded-[8px] bg-[#444444] px-[10px]">
+          {HEADER_CATEGORY_LIST.map(({ label, value }, index) => {
+            const isLastItem = index === HEADER_CATEGORY_LIST.length - 1;
+            return (
+              <Menu.Item key={index}>
+                {({ active }) => (
+                  <>
+                    <span
+                      className={"w-[48px] py-[8px] text-center font-medium"}
+                    >
+                      {label}
+                    </span>
+                    {!isLastItem && <DividerSelector />}
+                  </>
+                )}
+              </Menu.Item>
+            );
+          })}
+        </Menu.Items>
+      </Menu>
     </div>
   );
 };
---
+++ src/features/contacts/constants/headerCategoryList.ts
@@ -0,0 +1,20 @@
+import { TCategoryItem } from "@/features/contacts/type/TCategoryItem";
+
+export const HEADER_CATEGORY_LIST: TCategoryItem[] = [
+  {
+    label: "직장",
+    value: "WORK",
+  },
+  {
+    label: "거래처",
+    value: "BUYER",
+  },
+  {
+    label: "고객",
+    value: "CUSTOMER",
+  },
+  {
+    label: "기타",
+    value: "OTHERS",
+  },
+];
