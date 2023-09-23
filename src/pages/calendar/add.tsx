81669b0341d3a7666a184c5dbd0fe09d02ec2b68
jaychang99 <jaychang99@gmail.com>
2023-09-24 02:11:58 +0900

    feat: 일정 추가 완료

--- src/pages/calendar/add.tsx
+++ src/pages/calendar/add.tsx
@@ -4,6 +4,7 @@ import { UserSelectModal } from "@/components/userSelectModal/UserSelectModal";
 import { CONTACT_IMAGES } from "@/constants";
 import { TContactItem } from "@/features/contacts/type/TContactItem";
 import { useCreateCalendar } from "@/fetchers";
+import classNames from "classnames";
 import dayjs from "dayjs";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -28,9 +29,36 @@ const AddPage = () => {
     title: "",
     startDate: "",
     endDate: "",
-    category: "",
+    tags: [] as string[],
     memo: "",
   });
+
+  const handleSubmit = () => {
+    const apiFormValues = {
+      name: formValues.title,
+      start_dt: dayjs(formValues.startDate).toISOString(),
+      end_dt: dayjs(formValues.endDate).toISOString(),
+      is_all_day: true,
+      // remind_interval: 10,
+      is_important: true,
+
+      content: formValues.memo,
+      // is_complete: true,
+      // completed_at: "2023-09-23T14:23:32.341165Z",
+      // is_repeat: true,
+      tags: formValues.tags,
+      recurring_input: {
+        start_dt: "2023-09-23T17:01:52.781655Z",
+        end_dt: "2023-09-23T17:01:52.781691Z",
+        interval: 6,
+        frequency: "일",
+      },
+    };
+    trigger({
+      contactId: "14069e61-0ec5-49d1-98ba-ff411da6cefb",
+      calendar: apiFormValues as any,
+    });
+  };
 
   return (
     <div className="flex flex-col px-[20px]">
@@ -48,6 +76,12 @@ const AddPage = () => {
       <input
         className="mb-16 mt-8 bg-transparent text-[20px] font-bold placeholder-[#565656]"
         placeholder="일정 제목"
+        onChange={(e) => {
+          setFormValues({
+            ...formValues,
+            title: e.target.value,
+          });
+        }}
       />
       <div className="flex">
         <div className="mr-10 flex w-fit cursor-pointer items-center  rounded-[24px] border-1 border-solid border-[#84F44F] bg-[#353639] px-12 py-4">
@@ -136,8 +170,28 @@ const AddPage = () => {
       <div className="flex flex-row">
         {카테고리.map((item) => (
           <div
-            className="mr-10 flex w-fit cursor-pointer items-center rounded-[24px] bg-[#353639] px-12  py-4"
+            className={classNames(
+              "mr-10 flex w-fit cursor-pointer items-center rounded-[24px] bg-[#353639] px-12  py-4",
+              {
+                "border-1 border-solid border-[#84F44F]":
+                  formValues.tags.includes(item),
+              },
+            )}
             key={item}
+            onClick={() => {
+              if (formValues.tags.includes(item)) {
+                setFormValues({
+                  ...formValues,
+                  tags: formValues.tags.filter((tag) => tag !== item),
+                });
+                return;
+              }
+
+              setFormValues({
+                ...formValues,
+                tags: [...formValues.tags, item] as string[],
+              });
+            }}
           >
             {item}
           </div>
@@ -146,10 +200,21 @@ const AddPage = () => {
 
       {/* 메모 */}
       <div className="my-[16px] text-[15px] font-medium">메모</div>
-      <TextArea placeholder="기타 메모" />
+      <TextArea
+        placeholder="기타 메모"
+        onChange={(e) => {
+          setFormValues({
+            ...formValues,
+            memo: e.target.value,
+          });
+        }}
+      />
 
       {/* 완료하기 */}
-      <button className="bg-surface mb-[80px] mt-[40px] rounded-12 py-[15px] text-center text-[15px] text-[#fff] focus:outline-none">
+      <button
+        className="bg-surface mb-[80px] mt-[40px] rounded-12 py-[15px] text-center text-[15px] text-[#fff] focus:outline-none"
+        onClick={handleSubmit}
+      >
         완료하기
       </button>
     </div>
