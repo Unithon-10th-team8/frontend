b9daca9fde7889a5d9a08312d92361581dd1e918
jaychang99 <jaychang99@gmail.com>
2023-09-23 10:10:15 +0900

    feat(AppLayout): 초기 앱 레이아웃 설정

---
+++ src/components/common/AppLayout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const AppLayout = ({ children }: Props) => {
+  return (
+    <main className="relative mx-auto my-0 h-full  max-w-[393px] overflow-y-auto overflow-x-hidden bg-[#2b2c30]">
+      {children}
+    </main>
+  );
+};
+
+export default AppLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const { increaseCount, count } = useCount();
 
   return (
-    <div className="text-red-500 bg-white">
+    <div className=" text-white">
       <div className="flex">
         <button onClick={increaseCount}>카운트 증가</button>
       </div>
