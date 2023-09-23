d687587165b2c7ab3d3f534e8066b33b61665a61
jaychang99 <jaychang99@gmail.com>
2023-09-23 14:09:28 +0900

    refactor: 목업 데이터 적용

--- src/features/contacts/components/ContactsListContainer.tsx
+++ src/features/contacts/components/ContactsListContainer.tsx
@@ -1,12 +1,12 @@
 import { ContactItem } from "@/features/contacts/components/list/ContactItem";
+import { MOCKUP_CONTACT_LIST } from "@/features/contacts/mockups/MockupContactList";
 
 export const ContactsListContainer = () => {
   return (
     <div>
-      <ContactItem />
-      <ContactItem />
-      <ContactItem />
-      <ContactItem />
+      {MOCKUP_CONTACT_LIST.map((contact) => (
+        <ContactItem key={contact.id} {...contact} />
+      ))}
     </div>
   );
 };
--- src/features/contacts/components/list/ContactItem.tsx
+++ src/features/contacts/components/list/ContactItem.tsx
@@ -1,17 +1,18 @@
 import { ButtonContactBookmark } from "@/features/contacts/components/button/ButtonContactBookmark";
+import { TContactItem } from "@/features/contacts/type/TContactItem";
 
-const MOCKUP_TAGS = ["우아한형제들", "대리"];
+type Props = {} & TContactItem;
 
-export const ContactItem = () => {
+export const ContactItem = ({ id, name, tags }: Props) => {
   return (
     <div className=" border-b-solid flex items-center justify-between border-b-[1px] border-b-[#353639] pb-[15px] pl-[19px] pr-[20px] pt-[15px]">
       {/* TODO: 이미지 태그로 대체 */}
       <div className="flex">
         <div className="h-[58px] w-[58px] rounded-full bg-[#444]" />
         <div className="ml-[17px]">
-          <span className="text-[16px] font-[600]">김영한</span>
+          <span className="text-[16px] font-[600]">{name}</span>
           <div className="mt-[4px] flex flex-wrap gap-[7px]">
-            {MOCKUP_TAGS.map((tag, index) => (
+            {tags.map((tag, index) => (
               <span
                 key={index}
                 className="mr-[4px] bg-[#353639] px-[7px] py-[3px] text-[12px] font-[400] text-[#666]"
