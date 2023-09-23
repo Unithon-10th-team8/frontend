84e6edc7dcbc059e63eaf93e02beaae2b64e5e91
sangbooom <tkdqja4164@naver.com>
2023-09-23 21:12:04 +0900

    chore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import "@/styles/globals.css";
-import 'react-circular-progressbar/dist/styles.css';
+import "react-circular-progressbar/dist/styles.css";
 import { SWRConfig } from "swr";
 import type { AppProps } from "next/app";
 import { AppLayout, Navbar, ToastProvider } from "@/components";
+import useSignIn from "@/store/useSignIn";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { isSignedIn } = useSignIn();
   return (
     <SWRConfig>
       <AppLayout>
         <Component {...pageProps} />
-        <Navbar />
+        {isSignedIn && <Navbar />}
       </AppLayout>
       <ToastProvider />
     </SWRConfig>
