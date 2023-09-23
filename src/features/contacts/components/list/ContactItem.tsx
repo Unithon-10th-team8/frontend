3dbdbc1c1f9ee72a1a5287802befdc2a4547ea25
jaychang99 <jaychang99@gmail.com>
2023-09-24 01:17:14 +0900

    feat:(User): 유저 셀렉트 구현

--- src/features/contacts/components/list/ContactItem.tsx
+++ src/features/contacts/components/list/ContactItem.tsx
@@ -2,15 +2,35 @@ import { ButtonContactBookmark } from "@/features/contacts/components/button/But
 import { TContactItem } from "@/features/contacts/type/TContactItem";
 import Link from "next/link";
 
-type Props = {} & TContactItem;
+type Props = {
+  isSelectMode?: boolean;
+  setUser?: (user: TContactItem) => void;
+} & TContactItem;
 
-export const ContactItem = ({ id, name, tags }: Props) => {
+export const ContactItem = ({
+  id,
+  name,
+  tags,
+  setUser,
+  isSelectMode = false,
+}: Props) => {
   const contactDetailLink = `/contacts/${id}`;
+
+  const handleClick = () => {
+    if (!isSelectMode) return;
+
+    setUser?.({
+      id,
+      name,
+      tags,
+    });
+  };
 
   return (
     <Link
-      href={contactDetailLink}
+      href={isSelectMode ? "" : contactDetailLink}
       className=" border-b-solid flex animate-fade-in items-center justify-between border-b-[1px] border-b-[#353639] pb-[15px] pl-[19px] pr-[20px] pt-[15px]"
+      onClick={handleClick}
     >
       {/* TODO: 이미지 태그로 대체 */}
       <div className="flex">
@@ -29,7 +49,7 @@ export const ContactItem = ({ id, name, tags }: Props) => {
           </div>
         </div>
       </div>
-      <ButtonContactBookmark isBookmarked contactId={id} />
+      {!isSelectMode && <ButtonContactBookmark isBookmarked contactId={id} />}
     </Link>
   );
 };
