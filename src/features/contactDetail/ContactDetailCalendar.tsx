b400965ec963b6b414a1b8cfdaadfbc2f899e7e0
jaychang99 <jaychang99@gmail.com>
2023-09-23 22:38:20 +0900

    feat(User): 유저 페이지에 캘린더 추가

--- src/features/contactDetail/ContactDetailCalendar.tsx
+++ src/features/contactDetail/ContactDetailCalendar.tsx
@@ -2,7 +2,7 @@ import { Calendar } from "@/components";
 
 export const ContactDetailCalendar = () => {
   return (
-    <div>
+    <div className="bg-surface rounded-12">
       <Calendar />
     </div>
   );
