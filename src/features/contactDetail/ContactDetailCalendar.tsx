6690e619d19a9e2e7d48bc25c3f2a45a7e75a4b0
jaychang99 <jaychang99@gmail.com>
2023-09-24 04:23:21 +0900

    feat: 컨택트 캘린더 연동

--- src/features/contactDetail/ContactDetailCalendar.tsx
+++ src/features/contactDetail/ContactDetailCalendar.tsx
@@ -1,5 +1,7 @@
 import { Calendar } from "@/components";
+import { useGetCalendarByContactId } from "@/fetchers";
 import dayjs from "dayjs";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 const contacts = [
@@ -27,6 +29,24 @@ export const ContactDetailCalendar = () => {
   const [initialDate, setInitialDate] = useState(dayjs());
   const dateText = initialDate.format("D");
 
+  const { query } = useRouter();
+
+  const { data } = useGetCalendarByContactId(query.contactId as string, {
+    offset: 0,
+    limit: 100,
+  });
+
+  console.log("ddd", data);
+
+  // filter data that falls in to the range of start_dt and end_dt
+  const filteredData = data?.filter(
+    ({ start_dt, end_dt }) =>
+      dayjs(start_dt).isSame(initialDate, "day") ||
+      dayjs(end_dt).isSame(initialDate, "day") ||
+      (dayjs(start_dt).isBefore(initialDate, "day") &&
+        dayjs(end_dt).isAfter(initialDate, "day")),
+  );
+
   return (
     <div className="bg-surface rounded-12 pb-20">
       <Calendar
@@ -34,18 +54,23 @@ export const ContactDetailCalendar = () => {
           setInitialDate(date);
         }}
         initialDate={initialDate}
+        data={data}
       />
       <div className="mt-4 px-20">
         <p className="pb-8 text-[15px]">{dateText}일</p>
         <ul className="flex flex-col gap-16">
-          {contacts.map(({ bgColor, schedule, contents, id }) => (
+          {filteredData?.map(({ is_important, name, id }) => (
             <li
               className="flex w-full items-center gap-10 rounded-12 bg-[#444444] px-16 py-[15px]"
               key={id}
             >
-              <div className={`h-[9px] w-[9px] rounded-[50%] ${bgColor}`} />
-              <div>{schedule}</div>
-              <div>{contents}</div>
+              <div
+                className={`h-[9px] w-[9px] rounded-[50%] ${
+                  is_important ? "bg-accentGreen" : "bg-accentBlue"
+                }`}
+              />
+              <div>{name || "[제목 없음]"}</div>
+              {/* <div>{contents}</div> */}
             </li>
           ))}
         </ul>
--- src/fetchers/calendar/useGetAllCalendars.ts
+++ src/fetchers/calendar/useGetAllCalendars.ts
@@ -1,4 +1,19 @@
+import { CalendarOutput, APIClient } from "@/api";
 import useSWR from "swr";
 
-// TODO: API 명세 변경예정
-export const useGetCalendars = () => useSWR([]);
+type FetcherKey = readonly [[string, string], string, string];
+
+const fetcher = async ([, contactId, calendarId]: FetcherKey) => {
+  const result = await APIClient.Calendar.ge(contactId, calendarId);
+  return result.data;
+};
+
+export const useGetAllCalendars = (contactId: string, calendarId: string) =>
+  useSWR<CalendarOutput, any, FetcherKey>(
+    [
+      ["GET", "/v1/contacts/{contact_id}/calendars/{calendar_id}"],
+      contactId,
+      calendarId,
+    ],
+    fetcher,
+  );
