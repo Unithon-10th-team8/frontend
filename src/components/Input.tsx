e192c20343760eaa8063ffd4162a423bcc41f16b
jaychang99 <jaychang99@gmail.com>
2023-09-24 09:11:56 +0900

    feat: 문자 보내기, 메일 보내기 기능 구현

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,7 +15,7 @@ export const Input = ({ className, variant, ...props }: InputProps) => {
       autoCapitalize="off"
       autoCorrect="off"
       className={classNames(
-        "rounded-12 bg-surface px-16 py-[15px] text-[15px] text-white placeholder:text-[15px] placeholder:text-[#696969] autofill:bg-surface hover:autofill:bg-surface focus:outline-none focus:autofill:bg-surface active:autofill:bg-surface",
+        "bg-surface autofill:bg-surface hover:autofill:bg-surface focus:autofill:bg-surface active:autofill:bg-surface rounded-12 px-16 py-[15px] text-[15px] text-white placeholder:text-[15px] placeholder:text-[#696969] focus:outline-none",
         {
           "!rounded-b-none": variant === "start",
           "!rounded-t-none": variant === "end",
@@ -32,7 +32,7 @@ export type InputGroupProps = {
 
 export const InputGroup = ({ children }: InputGroupProps) => {
   return (
-    <div className="flex w-full flex-col rounded-12 bg-surface">{children}</div>
+    <div className="bg-surface flex w-full flex-col rounded-12">{children}</div>
   );
 };
 
